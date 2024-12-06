ad2e2cbf862f9e014dade9f0fd79620420707254
Szymon Jaroslawski <szymonixzebce@gmail.com>
2024-12-06 17:36:53 +0100

    Add fileserver hits counter and "/metrics", "/reset" endpoints

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
-func main() {
-	serve()
+type apiConfig struct {
+	fileserverHits atomic.Int32
 }
 
-func serve() {
+func main() {
+	config := &apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+	serve(config)
+}
+
+func serve(cfg *apiConfig) {
 	const PORT = "8080"
 
 	mux := http.NewServeMux()
@@ -23,6 +32,17 @@ func serve() {
 		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc("GET /metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+	})
+
+	mux.HandleFunc("POST /reset", func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Store(0)
+		w.WriteHeader(http.StatusOK)
 	})
 
 	srv := &http.Server{
