1cfa13c323b52577de0cc07026b141fa81b0768a
Szymon Jaroslawski <szymonixzebce@gmail.com>
2024-12-06 17:32:24 +0100

    Add health endpoint

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ func serve() {
 
 	mux.Handle("/app/", fileserverHanlder)
 
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
